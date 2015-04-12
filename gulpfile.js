d2a8b74c31ad67f21f44970d73e26ca082dc62c3
Christopher Hunt <chrahunt@gmail.com>
2015-04-12 17:10:44 -0400

    Easier userscript generation.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,15 @@ var concat = require("gulp-concat");
 var template = require("gulp-template");
 var jison = require("gulp-jison");
 var package_info = require("./package.json");
+var url = require("url");
+
+// Get the URL of the raw file as it will be available at the specified
+// Github repository on the master branch.
+function getGithubRawURL(repo, filePath) {
+  var githubRawBase = "https://raw.githubusercontent.com";
+  return url.resolve([githubRawBase, repo, "master"].join("/") + "/",
+    filePath);
+}
 
 // Build parser.
 gulp.task("jison", function() {
@@ -20,8 +29,16 @@ gulp.task("jison", function() {
 gulp.task("build", ["jison"], function() {
   return gulp.src(["./src/header.user.js", "./src/macro.js"])
     .pipe(concat("macro.user.js"))
-    .pipe(template({version: package_info.version}))
+    .pipe(template({
+      version: package_info.version,
+      parser: getGithubRawURL(package_info.repository, './dist/parser.js'),
+      updateUrl: getGithubRawURL(package_info.repository, './dist/macro.user.js')
+    }))
     .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("build-dev", ["jison"], function() {
+
 });
 
 gulp.task("default", ["build"]);
--- src/header.user.js
+++ src/header.user.js
@@ -2,7 +2,8 @@
 // @name          TagPro Neomacro Plus
 // @namespace     http://reddit.com/user/snaps_
 // @description   Better macros for a brighter future.
-// @require       https://gist.github.com/chrahunt/4843f0258c516882eea0/raw/loopback.user.js
+// @require       <%= parser %>
+// @downloadURL   <%= updateUrl %>
 // @include       http://tagpro-*.koalabeast.com:*
 // @include       http://maptest*.newcompte.fr:*
 // @license       MIT
